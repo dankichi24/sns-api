892008ae6a79205e5a7282b9198703a39f0854be
dankichi <dan24.yukito@gamil.com>
2025-04-12 20:28:29 +0900

    Vacelの変更

--- index.js
+++ index.js
@@ -38,7 +38,4 @@ app.use((req, res) => {
   res.status(404).json({ error: "ページが見つかりません" });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  // console.log(`🚀 Server is running on port ${PORT}`);
-});
+module.exports = app;
